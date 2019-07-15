1520bc013bc9c6d1c6d653c84cf6280363b95744
Sammy <samet.karaca.no@gmail.com>
2019-07-16 00:07:37 +0200

    Until componentDidMount()

---
+++ src/components/Recipe.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Recipe extends Component{
+
+    state ={
+        activeRecipe: []
+    }
+
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default Recipe
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from "./components/Router";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "./App";
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Router />, document.getElementById('root'));
